8fcd4403aab45f863db94250ed895513af71e9a1
AdrianBravent <102293927+AdrianBravent@users.noreply.github.com>
2022-11-25 12:50:22 +0100

    scene selector

---
+++ Editor/SceneSelectorOverlay.cs
@@ -0,0 +1,27 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.Overlays;
+using UnityEditor.Toolbars;
+using System;
+using UnityEngine.UIElements;
+using System.IO;
+using UnityEditor.SceneManagement;
+
+[Overlay(typeof(SceneView), "Scene Selector")]
+public class SceneSelectorOverlay : Overlay
+{
+    public override VisualElement CreatePanelContent()
+    {
+        VisualElement root = new VisualElement();
+
+        foreach (EditorBuildSettingsScene sceneSelector in EditorBuildSettings.scenes)
+        {
+            var button = new Button();
+            button.clicked += () => EditorSceneManager.OpenScene(sceneSelector.path);
+            button.text = Path.GetFileNameWithoutExtension(sceneSelector.path);
+            root.Add(button);
+        }
+        
+        return root;
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Scene selector overlay
+
+Simple overlay for Unity Editor to switch between all the scenes in build settings.
